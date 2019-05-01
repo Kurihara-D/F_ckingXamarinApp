888aaa093326c0dc8696c62269c856b207074651
Kurihara-D <na.ur666@gmail.com>
2019-05-01 23:02:18 +0900

    first commit

---
+++ Todo/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.Net.Http;
+using System.Runtime.Serialization.Json;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace Todo
+{
+    [DesignTimeVisible(true)]
+    public partial class MainPage : ContentPage
+    {
+        public List<TodoEntitiy> taskList;
+
+        public bool isBusy { get; set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            fetchTasks(new TodoAPI());
+        }
+        async void fetchTasks(TodoAPI api)
+        {
+            try
+            {
+                this.isBusy = true;
+                taskList = await api.AsyncGetWebAPIData();
+
+                var items = new List<String>();
+                foreach (TodoEntitiy value in taskList)
+                {
+                    items.Add(value.body);
+                }
+                this.BindingContext = taskList;
+            }
+            catch (System.Exception ex)
+            {
+                await DisplayAlert("Error", ex.Message.ToString(), "OK");
+            }
+            finally
+            {
+                this.isBusy = false;
+            }
+        }
+    }
+}
---
+++ Todo/TodoAPI.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.Net.Http;
+using System.Runtime.Serialization.Json;
+using Newtonsoft.Json;
+
+namespace Todo
+{
+    public class TodoAPI
+    {
+        public List<TodoEntitiy> taskList;
+
+        public string API_URL = "<some api url>";
+
+        public async Task<List<TodoEntitiy>> AsyncGetWebAPIData()
+        {
+            taskList = new List<TodoEntitiy>();
+
+            HttpClient httpClient = new HttpClient();
+
+            Task<string> stringAsync = httpClient.GetStringAsync(API_URL);
+            string result = await stringAsync;
+
+            taskList = JsonConvert.DeserializeObject<List<TodoEntitiy>>(result);
+
+            return taskList;
+        }
+    }
+
+    public class TodoEntitiy
+    {
+        public string id { get; set; }
+        public string body { get; set; }
+
+    }
+}
